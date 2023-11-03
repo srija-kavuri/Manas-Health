7377a860fab124d7efce7e2db70294fa7b334e9a
valli <vallivirat999@gmail.com>
2023-11-03 22:45:53 +0530

    Added image slideshow

---
+++ main.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+
+DisplaySlides();
+
+function DisplaySlides()
+{
+    var slides = document.getElementsByClassName("image");
+
+    var dots = document.getElementsByClassName("dot");
+
+    for (var i = 0; i < slides.length; i += 1)
+    {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length)
+    {
+        slideIndex = 1;
+    }
+
+    for (var i = 0; i < dots.length; i += 1)
+    {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";
+
+    dots[slideIndex-1].className += " active";
+
+    setTimeout(DisplaySlides, 3000); //changes the image every 3 seconds
+}
