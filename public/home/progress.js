b54da9a441d23166322badf7321d45f912d5f0b2
valli <vallivirat999@gmail.com>
2024-03-03 23:25:33 +0530

    changes progress page styling

--- public/home/progress.js
+++ public/home/progress.js
@@ -1,79 +1,83 @@
-function logout() {
-  fetch('/api/logout', {
-    method:'GET',
-    headers:{
-      'Content-Type': 'application/json',
-    },
-  })
-  .then(response=>{
-    if(response.ok){
-    window.location.replace('/login');
-    }else{
-     alert("Response is not ok");
-    }
-  })
-  .catch(error=>{
-    alert("Error logging out. Please try again.");
-    console.log(error);
-  })
-}
-const urlParams = new URLSearchParams(window.location.search);
-let student;
-if(urlParams){
-  student = encodeURIComponent(urlParams.get('student'));
-}
+// function logout() {
+//   fetch('/api/logout', {
+//     method:'GET',
+//     headers:{
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//   .then(response=>{
+//     if(response.ok){
+//     window.location.replace('/login');
+//     }else{
+//      alert("Response is not ok");
+//     }
+//   })
+//   .catch(error=>{
+//     alert("Error logging out. Please try again.");
+//     console.log(error);
+//   })
+// }
+// const urlParams = new URLSearchParams(window.location.search);
+// let student;
+// if(urlParams){
+//   student = encodeURIComponent(urlParams.get('student'));
+// }
 
 
 
-// Graph
-document.addEventListener('DOMContentLoaded',async function() {
-  let studentResults;
+// // Graph
+// document.addEventListener('DOMContentLoaded',async function() {
+//   let studentResults;
 
-  // Get the canvas element
-  var ctx = document.getElementById('myChart').getContext('2d');
-await fetch(`/api/student/progress/?student=${student}`,{
-  method: 'GET',
-  headers:{
-    'Content-Type': 'application/json',
-  },
-}).then(response=>response.json())
-.then(data=> {
-  if(data.success){
-    studentResults = data.progress;
-    console.log(studentResults);
-  }
-})
-//   studentResults = [
-//       { category: 'stress', score: 2, date: '22-03-2005' },
-//       { category: 'depression', score: 1, date: '23-04-2004' },
-//       { category: 'adhd', score: 1, date: '26-05-2003' },
-//       { category: 'general_test', score: 1, date: '20-05-1999' }
-//   ];
+//   // Get the canvas element
+// await fetch(`/api/student/progress/?student=${student}`,{
+//   method: 'GET',
+//   headers:{
+//     'Content-Type': 'application/json',
+//   },
+// }).then(response=>response.json())
+// .then(data=> {
+//   if(data.success){
+//     studentResults = data.progress;
+//     console.log(studentResults);
+//   }
+// })
+var ctx = document.getElementById('myChart').getContext('2d');
+
+  studentResults = [
+      { category: 'stress', percentage: 2, date: '22-03-2005' },
+      { category: 'depression', percentage: 1, date: '23-04-2004' },
+      { category: 'adhd', percentage: 1, date: '26-05-2003' },
+      { category: 'general_test', percentage: 1, date: '20-05-1999' }
+  ];
+
 
   // Filter the data to exclude 'general_test'
   const filteredList = studentResults.filter(obj => obj.category !== 'general_test');
 
   // Define data for the chart
   var categorylist = [];
-  var scorelist = [];
+  var percentagelist = [];
   var datelist = filteredList.map(item => item.date);
 
   filteredList.forEach(obj => {
       const capitalizedCategory = obj.category.charAt(0).toUpperCase() + obj.category.slice(1);
       // Store the capitalized category and score in their respective arrays
       categorylist.push(capitalizedCategory);
-      scorelist.push(obj.percentage);
+      percentagelist.push(obj.percentage);
   });
 
+  var labelWithDate = categorylist.map((category, index) => `${category} - ${datelist[index]}`);
   var data = {
-      labels: categorylist,
+      labels: labelWithDate,
       datasets: [{
           label: 'Percentage',
-          data: scorelist,
+          data: percentagelist,
           backgroundColor: 'skyblue',
           borderWidth: 1,
           date: datelist
-      }]
+      },
+    ]
   };
 
   // Create the chart
@@ -92,12 +96,12 @@ await fetch(`/api/student/progress/?student=${student}`,{
                   }
               },
               y: {
+                  max:100,
+                  grid:{
+                    display:false,
+                  },
                   ticks: {
-                      callback: function(value, index, values) {
-                          // Map numerical values to custom labels
-                          const labels = ['Normal', 'Mild', 'Moderate', 'Severe', 'Extremely Severe'];
-                          return labels[value - 1]; // Subtract 1 because arrays are zero-indexed
-                      }
+                      stepSize:10
                   }
               }
           },
@@ -105,8 +109,8 @@ await fetch(`/api/student/progress/?student=${student}`,{
               legend: {
                   display: true,
                   position: 'top',
-                 
-              }
+              },
+              
           },
           layout: {
               padding: {
@@ -116,18 +120,6 @@ await fetch(`/api/student/progress/?student=${student}`,{
                   bottom: 0
               }
           },
-          scales: {
-              x: {
-                  grid: {
-                      display: false
-                  }
-              },
-              y: {
-                  grid: {
-                      display: false
-                  }
-              }
-          }
       }
   });
-});
+// });
